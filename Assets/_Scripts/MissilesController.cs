ceece8f5054ba55b69a7fc129ad49784c8cabf67
Jan Mesarč <jame_581@windowslive.com>
2022-01-27 20:25:35 +0100

    Animation improved, health and hit from missiles added

--- Assets/_Scripts/MissilesController.cs
+++ Assets/_Scripts/MissilesController.cs
@@ -3,30 +3,49 @@ using UnityEngine;
 
 public class MissilesController : MonoBehaviour
 {
-    GameObject smokeEffect;
+    Animator animator;
+
+    bool readyToBlow = false;
+
+    bool missilesBlowed = false;
+
+    public bool MissilesBlowed { get => missilesBlowed; }
 
     void Awake()
     {
-        smokeEffect = transform.GetChild(0).gameObject;
+        animator = GetComponent<Animator>();
     }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.CompareTag("Player"))
+        if (collider.CompareTag("Player") && readyToBlow)
         {
+            readyToBlow = false;
+            StartCoroutine(DestroyObject());
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player") && readyToBlow)
+        {
+            readyToBlow = false;
             StartCoroutine(DestroyObject());
         }
     }
 
     IEnumerator DestroyObject()
     {
-        if (smokeEffect)
-        {
-            smokeEffect.SetActive(true);
-        }
+        animator.SetBool("Blow", true);
+        missilesBlowed = true;
 
         yield return new WaitForSeconds(0.8f);
 
         Destroy(gameObject);
+    }
+
+    void SetReadyToBlow()
+    {
+        readyToBlow = true;
     }
 }
--- Assets/_Scripts/SubmarineController.cs
+++ Assets/_Scripts/SubmarineController.cs
@@ -4,40 +4,52 @@ public class SubmarineController : MonoBehaviour
 {
     [SerializeField]
     float moveHorizontalForce = 100f; // Amount of force added to move the player
-    
+
     [SerializeField]
     float moveVerticalForce = 100f; // Amount of force added to move the player
 
     [SerializeField]
     float maxHorizontalSpeed = 2f;
-    
+
     [SerializeField]
     float maxVerticalSpeed = 2f;
 
     [SerializeField]
+    float moveAnimationSpeed = 0.6f;
+
+    [SerializeField]
+    float baseAnimationSpeed = 0.2f;
+
+    [SerializeField]
     bool facingRight;   // For determining which way the player is currently facing.
+
 
     Rigidbody2D rigidBody;
 
+    Animator animator;
+
     Vector2 direction;
+
+    int health = 3;
+
+    bool hitTaken = false;
 
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
         if (rigidBody == null) return;
+        if (animator == null) return;
 
         direction.x = Input.GetAxis("Horizontal");
         direction.y = Input.GetAxis("Vertical");
+
+        float animationSpeed = Mathf.Abs(direction.x);
+        animator.SetFloat("Speed", animationSpeed == 0 ? baseAnimationSpeed : moveAnimationSpeed);
 
         // If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
         if (direction.x * rigidBody.velocity.x < maxHorizontalSpeed)
@@ -52,8 +64,9 @@ public class SubmarineController : MonoBehaviour
 
         // If the input is moving the player right and the player is facing left...
         if (direction.x > 0 && !facingRight || direction.x < 0 && facingRight)
-            Flip(); 
+            Flip();
     }
+
     void Flip()
     {
         // Switch the way the player is labelled as facing.
@@ -63,5 +76,52 @@ public class SubmarineController : MonoBehaviour
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+    }
+
+    void TakeHit()
+    {
+        if (hitTaken) return;
+            
+        hitTaken = true;
+
+        health--;
+
+        if (health <= 0)
+        {
+            // TODO: EndGame
+        }
+
+        Invoke("TurnOffImunity", 3.0f);
+    }
+
+    void TurnOffImunity()
+    {
+        hitTaken = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Missiles"))
+        {
+            MissilesController missiles = collider.gameObject.GetComponent<MissilesController>();
+            if (missiles != null && missiles.MissilesBlowed)
+            {
+                animator.SetTrigger("Hit");
+                TakeHit();
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Missiles"))
+        {
+            MissilesController missiles = collider.gameObject.GetComponent<MissilesController>();
+            if (missiles != null && missiles.MissilesBlowed)
+            {
+                animator.SetTrigger("Hit");
+                TakeHit();
+            }
+        }
     }
 }
