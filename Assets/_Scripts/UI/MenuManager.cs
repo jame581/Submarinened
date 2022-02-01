e4154f1df1b9b30c0c97b35f73cfbc2b671369f9
Jan Mesarč <jame_581@windowslive.com>
2022-02-01 16:10:06 +0100

    Main Menu

---
+++ Assets/_Scripts/UI/MenuManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject CreditsPanel;
+
+    void Start()
+    {
+        if (CreditsPanel == null)
+            Debug.LogError($"CreditsPanel reference missing on {gameObject.name}!", this);
+        
+        CreditsPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            CreditsPanel.SetActive(false);
+        }
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    public void ShowCredits()
+    {
+        CreditsPanel.SetActive(true);
+    }
+
+    public void HideCredits()
+    {
+        CreditsPanel.SetActive(false);
+    }
+}
